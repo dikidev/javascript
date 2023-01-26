d3ffe54237378ee5c5098280e3f9efa143130232
D <d@diki.dev>
2023-01-26 20:27:37 +0000

    Adding Assignment number ++

---
+++ variable.js
@@ -0,0 +1,14 @@
+let myString = "Hello, World!";
+let myBoolean = true;
+let myUndefined;
+let myNull = null;
+
+console.log(typeof myString); // "string"
+console.log(typeof myBoolean); // "boolean"
+console.log(typeof myUndefined); // "undefined"
+console.log(typeof myNull); // "object"
+
+console.log(myString); // "Hello, World!"
+console.log(myBoolean); // true
+console.log(myUndefined); // undefined
+console.log(myNull); // null
